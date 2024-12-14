3c993fbf9710e00b58288142d1f49bd8c1cfe3e0
Davide Bresolin <bresolin.davide@gmail.com>
2024-12-14 21:01:27 +0100

    Day 13, very fast solution

--- day13/day13.py
+++ day13/day13.py
@@ -1,24 +1,20 @@
-import heapq
 import re
+import sys
 
-def shortest_path(xa, ya, xb, yb, xp, yp):
-    heap = [(0, 0, 0, 'A')]
-    heapq.heapify(heap)
-    while len(heap) > 0:
-        cost, x, y, btn = heapq.heappop(heap)
-        #print("Popped", cost, x, y, btn)
-        if x > xp or y > yp:
-            continue
-        if x == xp and y == yp:
-            return cost
-        # Press button A only if button is not B
-        if btn == 'A':
-            heapq.heappush(heap, (cost + 3, x + xa, y + ya, 'A'))
-        # Press button B
-        heapq.heappush(heap, (cost + 1, x + xb, y + yb, 'B'))
-    return 0
+def fastest_sp(xa, ya, xb, yb, xp, yp):
+    nb = (xa * yp - ya * xp)
+    db = (xa * yb - xb * ya)
+    (b, q) = divmod(nb, db)
+    if q != 0:
+        return 0
+    na = (xb * yp - yb * xp)
+    da = (xb * ya - xa * yb)
+    (a, q) = divmod(na, da)
+    if q != 0:
+        return 0
+    return 3*a + b
 
-infile = open("input.txt")
+infile = open(sys.argv[1])
 
 tokens = 0
 while row := infile.readline():
@@ -27,6 +23,7 @@ while row := infile.readline():
     xb, yb = (int(s) for s in re.split("\+|=|,| |\t|\n", row) if s.isdigit() )
     row = infile.readline()
     xp, yp = (int(s) for s in re.split("\+|=|,| |\t|\n", row) if s.isdigit() )
-    tokens += shortest_path(xa, ya, xb, yb, xp, yp)
-    row = infile.readline()   
+    ofs = 10000000000000
+    tokens += fastest_sp(xa, ya, xb, yb, xp + ofs, yp + ofs)
+    row = infile.readline()
 print(tokens)
