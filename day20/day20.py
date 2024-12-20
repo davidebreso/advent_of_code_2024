4c7921372778633633f18459350c81a4aeb052a7
Davide Bresolin <bresolin.davide@gmail.com>
2024-12-20 21:46:26 +0100

    Day 20 part 2

--- day20/day20.py
+++ day20/day20.py
@@ -56,6 +56,40 @@ def find_cheats(minsave, costs):
                 if save >= minsave:
                     cheats.append(save)             
     return cheats
+    
+def find_longer_cheats(start, minsave, maxsteps, costs):
+    rows = len(costs)
+    cols = len(costs[0])
+    cheats = list()
+    startcost = costs[start[0]][start[1]]
+    heap = [(0, start[0], start[1])]
+    heapq.heapify(heap)
+    visited = set()
+    while len(heap) > 0:
+        w, r, c = heapq.heappop(heap)
+        if (r, c) in visited:
+            continue
+        visited.add((r, c))
+        if w > maxsteps:
+            continue
+        if costs[r][c] >= 0:
+            save = startcost - costs[r][c] - w
+            if save >= minsave:
+                cheats.append((save, start, (r, c)))
+        for (dr, dc) in directions:
+            if 0 < r + dr < rows - 1 and 0 < c + dc < cols - 1:
+                heapq.heappush(heap, (w + 1, r + dr, c + dc))
+    return cheats 
+    
+def find_all_cheats(minsave, maxsteps, costs):
+    rows = len(costs)
+    cols = len(costs[0])
+    cheats = list()
+    for r in range(1, rows - 1):
+        for c in range(1, cols - 1):
+            if costs[r][c] > 0:
+                cheats.extend(find_longer_cheats((r, c), minsave, maxsteps, costs))
+    return cheats
 
 if len(sys.argv) != 2:
     print("Usage:", sys.argv[0], "inputfile")
@@ -65,10 +99,10 @@ infile = open(sys.argv[1])
 grid = [ list(row.strip()) for row in infile ]
 
 start, end = find_endpoints(grid)
-print_grid(grid)
 costs = compute_shortest_paths(end, grid)
 print("Lowest score with no cheating from", start, "to", end, "is", costs[start[0]][start[1]])
 cheats = find_cheats(100, costs)
 print(len(cheats), "cheats saves at least 100 picoseconds")
-
+cheats = find_all_cheats(100, 20, costs)
+print(len(cheats), "longer cheats saves at least 100 picoseconds")
 
